e7f80ce3492b46c8fc754ab60406d1b95d1984e4
RenanLima7 <renanlima.developer@gmail.com>
2025-07-30 23:57:28 -0300

    refacto: Remove Environment

--- UniHub/UniHub.API/Program.cs
+++ UniHub/UniHub.API/Program.cs
@@ -58,11 +58,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
